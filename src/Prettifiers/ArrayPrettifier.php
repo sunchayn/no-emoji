c4765bab143186d054e21856b2562d87685f7c47
Mazen Touati <mazen_touati@hotmail.com>
2019-02-06 22:53:37 +0100

    Fix File namespace

--- src/Prettifiers/ArrayPrettifier.php
+++ src/Prettifiers/ArrayPrettifier.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace MazenTouati\NoEmoji\Prettifiers;
 
+use MazenTouati\NoEmoji\Entities\File;
+
 /**
  * @author Mazen Touati <mazen_touati@hotmail.com>
  */
@@ -11,11 +13,11 @@ class ArrayPrettifier
     /**
      * Prettify the input
      *
-     * @param  File $file The file instance
+     * @param File $file The file instance
      *
      * @return string
      */
-    public static function run($file)
+    public static function run(File $file)
     {
         $content = trim($file->content, '\'');
         $ranges = explode('|', $content);
